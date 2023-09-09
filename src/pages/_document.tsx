38203f0de3bfecd1cfd4e773c6f3c6d26b8198b8
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-09-09 10:03:59 -0600

    Add monetag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <meta name="monetag" content="3852b82d5855e54b525c1c821463e787" />
+      </Head>
       <body className={"dark:bg-[#1A1A1A] dark:text-gray text-dark"}>
         <Main />
         <NextScript />
