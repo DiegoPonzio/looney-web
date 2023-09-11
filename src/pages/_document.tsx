c026b117eb089219173e93c5725065493c44750c
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-09-11 17:05:16 -0600

    Refactor enviroments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,12 @@ export default function Document() {
       <body className={"dark:bg-[#1A1A1A] dark:text-gray text-dark"}>
         <Main />
         <NextScript />
-        <script src="https://alwingulla.com/88/tag.min.js" data-zone="9972" async data-cfasync="false"></script>
-        <script src="https://bydurantr.com/pfe/current/tag.min.js?z=6311852" data-cfasync="false" async></script>
+        {process.env.NODE_ENV === "production" && (
+            <>
+                <script src="https://alwingulla.com/88/tag.min.js" data-zone="9972" async data-cfasync="false"></script>
+                <script src="https://bydurantr.com/pfe/current/tag.min.js?z=6311852" data-cfasync="false" async></script>
+            </>
+        )}
       </body>
     </Html>
   )
